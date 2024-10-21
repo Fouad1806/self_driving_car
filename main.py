a27d288b02291da514f0dda20b55cf1f69280f78
Fouad Mokhtari <fouad.mokhtari@study.hs-duesseldorf.de>
2024-10-21 10:54:45 +0200

    Creating window logic

---
+++ main.py
@@ -0,0 +1,21 @@
+# Contains every presetting that I need and easily adjustable
+from settings import *
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption('Self Driving Car Simulator')
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            pygame.display.update()
+    
+
+game = Game()
+game.run()
+
